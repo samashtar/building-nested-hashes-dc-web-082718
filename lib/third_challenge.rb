79959331a2863be7ca64f1f9835e2f635b676012
Sam Ashtar <samashtarm@gmail.com>
2018-08-22 03:20:08 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,14 +13,11 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-:name => "Lord Capulet", :age => "50"    
+      :patriarch => {:name => "Lord Capulet", :age => "50"    
 },
-      :matriarch => {
-:name => "Lady Capulet", :age => "51"   
+      :matriarch => {:name => "Lady Capulet", :age => "51"   
 },
-      :heroine => {
-:name => "Juliet", :age => "15", :status = "alive"
+      :heroine => {:name => "Juliet", :age => "15", :status = "alive"
 },
       :heroine_friends => []
    }
